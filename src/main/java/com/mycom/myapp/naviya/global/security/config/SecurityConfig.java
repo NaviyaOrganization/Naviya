af015ac7b5d3b73d99f5e7566e397c73bee0813e
lixxce5017 <lixxce5017@gmail.com>
2024-10-22 20:56:23 +0800

    [feat]book detail 변경,도서 쿼리들 나이대별로 들고오게 수정,좋아요 논리적 삭제 구현,좋아요-도서 관계 수정

--- src/main/java/com/mycom/myapp/naviya/global/security/config/SecurityConfig.java
+++ src/main/java/com/mycom/myapp/naviya/global/security/config/SecurityConfig.java
@@ -21,7 +21,9 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/login", "/signup", "/signupProc",
-                                        "/static/**", "/css/**", "/js/**", "/images/**").permitAll()
+                                "/static/**", "/css/**", "/js/**", "/images/**").permitAll()
+                        .requestMatchers("/Book/**").permitAll() // API 요청에 대한 접근 허용
+
                         .anyRequest().authenticated()
                 )
                 .formLogin(auth -> auth
