7eea21c23be7248dc3fbcecf081bbc1364c1995d
jun10920 <abide0206@naver.com>
2024-10-22 17:14:18 +0900

    :art: [Enhancement] 응모시 이름과 전화번호 제약 사항 추가

--- src/main/java/com/mycom/myapp/naviya/domain/lottery/dto/LotteryEntryRequest.java
+++ src/main/java/com/mycom/myapp/naviya/domain/lottery/dto/LotteryEntryRequest.java
@@ -1,11 +1,18 @@
 package com.mycom.myapp.naviya.domain.lottery.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class LotteryEntryRequest {
+
+    @NotBlank(message = "이름은 필수 입력 사항입니다.")
     private String name;  // 사용자 이름
+
+    @NotBlank(message = "전화번호는 필수 입력 사항입니다.")
+    @Pattern(regexp = "^\\d{11}$", message = "전화번호는 11자리 숫자여야 합니다.")
     private String phone; // 사용자 전화번호
 }
