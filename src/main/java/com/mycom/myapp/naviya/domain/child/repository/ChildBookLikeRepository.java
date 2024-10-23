726fe6ce5a02b99dd9f4712d19e658f737acfb32
lixxce5017 <lixxce5017@gmail.com>
2024-10-23 17:33:36 +0800

    feat_동적가중치조정 mbti 추가 적용 및 좋아요 싫어요 메서드 추가수정

--- src/main/java/com/mycom/myapp/naviya/domain/child/repository/ChildBookLikeRepository.java
+++ src/main/java/com/mycom/myapp/naviya/domain/child/repository/ChildBookLikeRepository.java
@@ -21,4 +21,12 @@ public interface ChildBookLikeRepository extends JpaRepository<ChildBookLike, Lo
             "WHERE cbl.child.childId = :childId AND cbl.book.bookId = :bookId AND cbl.DelDate IS NULL")
     boolean existsByChildIdAndBookIdAndDelDateIsNull(@Param("childId") Long childId, @Param("bookId") Long bookId);
 
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM ChildBookLike c " +
+            "WHERE c.child.childId = :childId " +
+            "AND c.book.bookId = :bookId " +
+            "AND c.DelDate IS NULL")
+    void deleteByChildIdAndBookIdAndDelDateIsNull(@Param("childId") Long childId, @Param("bookId") Long bookId);
 }
